b052fbf9ee584812c5709c986aaf9241765d795c
mm3176 <93787471+mm3176@users.noreply.github.com>
2025-04-27 14:24:22 +0100

    Implemented post creation. Posts show on homepage

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 let articles = [];
+let images = ["random_article1.jpg", "random_article2.jpg", "random_article3.jpg", "random_article4.jpg", "random_article5.jpg", "random_article6.jpg", "random_article7.jpg", "random_article8.jpg", "random_article9.jpg", "random_article10.jpg", "random_article11.jpg", "random_article12.jpg"]
+
+function getRandomImage(){
+  let imageChosen = images[Math.floor(Math.random() * 12)];
+  return imageChosen;
+}
 
 app.use(express.static("public"));
 
@@ -24,14 +30,14 @@ app.get("/about.ejs", (req, res) => {
 
  app.post("/submit", (req, res) => {
   const title = req.body["submitted-title"];
-  const image = "most-recent-article.jpg"
+  const image = getRandomImage();
   const imageDescription = req.body["submitted-image-description"];
   const summary = req.body["submitted-summary"];
   const article = req.body["submitted-article"];
 
   const newArticle = {
     title: title,
-    image: image,
+    articleImage: image,
     imageDescription: imageDescription,
     summary: summary,
     article: article
@@ -49,7 +55,7 @@ app.listen(port, () => {
 
   let firstArticle = {
     title: "Article Title",
-    articleImage: "article1.jpg",
+    articleImage: getRandomImage(),
     imageDescription: "Picture of a coffee",
     summary: "Fusce varius, justo eu ullamcorper congue, ante mi elementum massa, eget hendrerit odio elit in lacus. Nam lacinia nisi quis hendrerit tincidunt. Integer magna dui, bibendum quis tortor et, venenatis pretium justo.",
     article: "This is an example article"
@@ -57,7 +63,7 @@ app.listen(port, () => {
 
   let secondArticle = {
     title: "Article Title",
-    articleImage: "article2.jpg",
+    articleImage: getRandomImage(),
     imageDescription: "Lake landscape",
     summary: "Vivamus ut rutrum ex, quis feugiat dolor. Nam vel felis dui. Maecenas viverra ex vel augue porta, non porttitor mi condimentum. Integer lobortis sapien tortor, sed tempor eros interdum vel. Phasellus vel suscipit ligula.",
     article: "This is an example article"
@@ -65,7 +71,7 @@ app.listen(port, () => {
 
   let thirdArticle = {
     title: "Article Title",
-    articleImage: "article3.jpg",
+    articleImage: getRandomImage(),
     imageDescription: "Picture of a camera",
     summary: "Sed luctus enim non mauris sodales, ac molestie odio porttitor. In auctor ornare sodales. Mauris hendrerit dolor neque, id sodales purus porttitor eget. Vestibulum molestie dictum euismod.",
     article: "This is an example article"
